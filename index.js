7332351be4ed19c282f29df3d50a926158dab12b
AllredW <allrewil@gmail.com>
2024-07-26 01:19:06 -0400

    added checks for inquirer answers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Link relevant modules and scripts: inquirer, shapes.js, 
 const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: create object template for desired logo; properties include shape, color, text
 class Logo {
     constructor(shape, color, text, textColor) {
@@ -28,19 +29,30 @@ inquirer
         name: "text",
         message: "What text would you like to appear on your logo? Maximum of 3 characters.",
       },
-      // TODO: check that text has a max of 3 characters
-      if (authorName.length > 3) {
-        throw new Error('Text cannot be more than 3 characters long.');
-      },
+
       {
         type: "input",
         name: "color",
         message: "What color would you like your logo text to be? Enter a keyword, or use a hex code number.",
       },
 ]);
-
+      // TODO: check that text has a max of 3 characters
 
 // TODO: check that color/shape keywords are valid
 
 // TODO: generate logo by passing in inquirer data
-const newLogo = new Logo(`${shape}`,`${color}`, `${text}`, `${textColor}`,);
+.then((answers) => {
+    console.log(answers);
+    // Pass inquirer answers to generateMarkdown
+
+// checks that text length is less than 4 characters
+if (text.length > 3) {
+    throw new Error('Text must be no more than 3 characters long.');
+  }
+    // Regex for only allowing uppercase, lowercase, numbers, dashes, and underscores.
+    const pattern = new RegExp('([A-Za-z0-9_-]+$)');
+//   checks that both color keywords match a color
+
+//   generate new Logo with answer parameters
+    const newLogo = new Logo(`${shape}`,`${color}`, `${text}`, `${textColor}`,);
+  });
