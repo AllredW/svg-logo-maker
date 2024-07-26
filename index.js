613ae7664d3f350230da0850f269d9eaf34b8bd8
AllredW <allrewil@gmail.com>
2024-07-26 00:39:24 -0400

    pseudocoding and planning

---
+++ index.js
@@ -0,0 +1,11 @@
+// TODO: Link relevant modules and scripts: inquirer, shapes.js, 
+
+// TODO: create object template for desired logo; properties include shape, color, text
+
+// TODO inquirer prompt package: logo text, text color, logo shape, logo color
+
+// TODO: check that text has a max of 3 characters
+
+// TODO: check that color/shape keywords are valid
+
+// TODO: generate logo by passing in inquirer data
