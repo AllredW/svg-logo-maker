b08b928439409d6f6450c5eca0fd07d9677b16ef
AllredW <allrewil@gmail.com>
2024-07-26 15:18:44 -0400

    confirmed functionality of inquirer module

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-// TODO: Link relevant modules and scripts: inquirer, shapes.js, 
+// TODO: Link relevant modules and scripts: inquirer, shapes.js,
 const inquirer = require("inquirer");
 const fs = require("fs");
+const shapes = require("./lib/shapes.js");
 // TODO: create object template for desired logo; properties include shape, color, text
 class Logo {
-    constructor(shape, color, text, textColor) {
-        this.shape = shape;
-        this.color = color;
-        this.text = text;
-        this.textColor = textColor;
-    }
+  constructor(shape, color, text, textColor) {
+    this.shape = shape;
+    this.color = color;
+    this.text = text;
+    this.textColor = textColor;
+  }
 }
 // TODO inquirer prompt package: logo text, text color, logo shape, logo color
 inquirer
@@ -16,43 +17,54 @@ inquirer
     {
       type: "list",
       name: "shape",
-      message: "What shape would you like to use for your logo? Scroll with the arrow keys, and press Space to select a shape.",
+      message:
+        "What shape would you like to use for your logo? Scroll with the arrow keys, and press Space to select a shape.",
       choices: ["Circle", "Triangle", "Square"],
     },
     {
-        type: "input",
-        name: "color",
-        message: "What color would you like your logo to be? Enter a keyword, or use a hex code number.",
-      },
-      {
-        type: "input",
-        name: "text",
-        message: "What text would you like to appear on your logo? Maximum of 3 characters.",
-      },
+      type: "input",
+      name: "color",
+      message:
+        "What color would you like your logo to be? Enter a keyword, or use a hex code number.",
+    },
+    {
+      type: "input",
+      name: "text",
+      message:
+        "What text would you like to appear on your logo? Maximum of 3 characters.",
+    },
 
-      {
-        type: "input",
-        name: "color",
-        message: "What color would you like your logo text to be? Enter a keyword, or use a hex code number.",
-      },
-]);
-      // TODO: check that text has a max of 3 characters
+    {
+      type: "input",
+      name: "textColor",
+      message:
+        "What color would you like your logo text to be? Enter a keyword, or use a hex code number.",
+    },
+  ])
+  // TODO: check that text has a max of 3 characters
 
-// TODO: check that color/shape keywords are valid
-
-// TODO: generate logo by passing in inquirer data
-.then((answers) => {
+  // TODO: generate logo by passing in inquirer data
+  .then((answers) => {
     console.log(answers);
     // Pass inquirer answers to generateMarkdown
+    //     let shapeData;
+    // switch (answers.shape) {
+    //   case "circle":
+    //   // shapeData =  new Circle;
+    //     break;
 
-// checks that text length is less than 4 characters
-if (text.length > 3) {
-    throw new Error('Text must be no more than 3 characters long.');
-  }
-    // Regex for only allowing uppercase, lowercase, numbers, dashes, and underscores.
-    const pattern = new RegExp('([A-Za-z0-9_-]+$)');
-//   checks that both color keywords match a color
+    //     case "square":
+    //     // shapeData =  new Square;
+    //     break;
 
-//   generate new Logo with answer parameters
-    const newLogo = new Logo(`${shape}`,`${color}`, `${text}`, `${textColor}`,);
+    //   default:
+    //   // shapeData =  new Triangle;
+    //     break;
+    // }
+    // checks that text length is less than 4 characters
+    if (answers.text.length > 3) {
+      throw new Error("Text must be no more than 3 characters long.");
+    }
+    //   generate new Logo with answer parameters
+    // const newLogo = new Logo(`${shape}`,`${color}`, `${text}`, `${textColor}`,);
   });
